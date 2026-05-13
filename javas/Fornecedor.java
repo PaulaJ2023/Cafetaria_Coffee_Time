0ffb77d18bd95c5b85eca154e2cd7bd921a5bd1e
dudtlly <eduardgoncavestlly@gmail.com>
2026-05-12 21:57:09 -0300

    fiz o fornecedor

--- javas/Fornecedor.java
+++ javas/Fornecedor.java
@@ -1,5 +1,24 @@
 package javas;
 
-public class Fornecedor {
-    
+public class Fornecedor extends Cliente {
+    private String cnpj;
+    public Fornecedor(String nome, String cpf, String telefone, String endereco, double pagamento, String cnpj){
+        super(nome, cpf, telefone, endereco, pagamento);
+        this.cnpj = cnpj;
+    }
+    public String getCnpj(){
+        return cnpj;
+    }
+    public void setCnpj(String cnpj){
+        this.cnpj = cnpj;
+    }
+    @Override
+    public void exibirDados(){
+        System.out.println("Nome:" + getNome());
+        System.out.println("CPF:" + getCpf());
+        System.out.println("CNPJ:" + cnpj);
+        System.out.println("Telefone:" + getTelefone());
+        System.out.println("Endereço:" + getEndereco());
+        System.out.println("Pagamento:" + getPagamento());
+    }  
 }
